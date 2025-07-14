2bbef3847908ed5cfbb21525f99d38ed6a97af3e
Cameron Brown <savildatechnology@gmail.com>
2025-07-13 19:43:46 -0600

    Update dns-cert-runner.bash

--- dns-cert-runner.bash
+++ dns-cert-runner.bash
@@ -3,6 +3,7 @@
 ### Certbot requires root, unless you change settings (...and apt) ###
 [ ! "$(whoami)" == "root" ] && echo 'You must be root! try again...' && exit 1
 
+run_certbot='0' #change to '1' if you want to run certbot
 install_certbot='0' #change to '1' if you want to install certbot
 email='your_email@email_domain.com' #change this
 wait_seconds='180' #wait (seconds) for dns propagation
@@ -14,8 +15,6 @@ ini_file='/home/<user>/.secrets/cloudflare.ini'
 
 ### works on debian/ubuntu ###
 
-[ $# -lt 1 ] && echo 'you need to add your -d flag, followed by the (sub)domain you want, supports multiple -d entries' && exit 1
-
 if [ "$install_certbot" == '1' ]; then
   apt update
   [ $? -ne 0 ] && echo 'failed to update apt' && exit 1
@@ -25,7 +24,13 @@ if [ "$install_certbot" == '1' ]; then
   [ $? -ne 0 ] && echo 'failed to install python3-certbot' && exit 1
   apt install -y python3-certbot-dns-cloudflare
   [ $? -ne 0 ] && echo 'failed to install python3-certbot-dns-cloudflare' && exit 1
+else
+  echo "install skipped because install_certbot is not set to '1'."
 fi
+
+[ "$run_certbot" -ne '1' ] && echo "run_certbot is not set to '1', exiting..." && exit 0
+
+[ $# -lt 1 ] && echo 'you need to add your -d flag, followed by the (sub)domain you want, supports multiple -d entries' && exit 1
 
 certbot certonly -n --agree-tos \
 -m "$email" \
