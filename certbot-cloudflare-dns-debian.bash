0c9d57e554d17547a57610f63edcfc29e8b6cb94
Cameron Brown <savildatechnology@gmail.com>
2025-07-13 19:06:22 -0600

    Update certbot-cloudflare-dns-debian.bash

--- certbot-cloudflare-dns-debian.bash
+++ certbot-cloudflare-dns-debian.bash
@@ -3,10 +3,10 @@
 [ ! "$(whoami)" == "root" ] && echo 'You must be root! try again...' && exit 1
 
 apt update
-[ $? -ne 0 ] && echo 'failed to update apt' && return 1
+[ $? -ne 0 ] && echo 'failed to update apt' && exit 1
 apt upgrade -y
-[ $? -ne 0 ] && echo 'failed to upgrade apt' && return 1
+[ $? -ne 0 ] && echo 'failed to upgrade apt' && exit 1
 apt install -y python3-certbot
-[ $? -ne 0 ] && echo 'failed to install python3-certbot' && return 1
+[ $? -ne 0 ] && echo 'failed to install python3-certbot' && exit 1
 apt install -y python3-certbot-dns-cloudflare
-[ $? -ne 0 ] && echo 'failed to install python3-certbot-dns-cloudflare' && return 1
+[ $? -ne 0 ] && echo 'failed to install python3-certbot-dns-cloudflare' && exit 1
